e467c5cdf3186ea0641479d8ba17a471817d027c
alpha <sallypha@163.com>
2020-03-22 15:54:16 +0800

    1，添加gitignore 2，创建ExcelDemo

---
+++ SourceCode/ExcelDemo/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ExcelDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.writeExcelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // writeExcelButton
+            // 
+            this.writeExcelButton.Location = new System.Drawing.Point(42, 22);
+            this.writeExcelButton.Name = "writeExcelButton";
+            this.writeExcelButton.Size = new System.Drawing.Size(126, 23);
+            this.writeExcelButton.TabIndex = 0;
+            this.writeExcelButton.Text = "写入Excel";
+            this.writeExcelButton.UseVisualStyleBackColor = true;
+            this.writeExcelButton.Click += new System.EventHandler(this.writeExcelButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.writeExcelButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button writeExcelButton;
+    }
+}
+
---
+++ SourceCode/ExcelDemo/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ExcelDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeExcelButton_Click(object sender, EventArgs e)
+        {
+            string currentPath = Directory.GetCurrentDirectory();
+            WriteExcel(currentPath + "\\results.xlsx");
+
+            MessageBox.Show("Success!");
+        }
+
+
+        public void WriteExcel(string filename)
+        {
+            //new an excel object
+            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+            if (excelApp == null)
+            {
+                // if equal null means EXCEL is not installed.
+                MessageBox.Show("Excel is not properly installed!");
+                return;
+            }
+
+            // open a workbook,if not exist, create a new one
+            Microsoft.Office.Interop.Excel.Workbook workBook;
+            if (File.Exists(filename))
+            {
+                workBook = excelApp.Workbooks.Open(filename, 0, false, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            }
+            else
+            {
+                workBook = excelApp.Workbooks.Add(true);
+            }
+
+            //new a worksheet
+            var workSheet = workBook.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+
+            //write data
+            workSheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.Worksheets.get_Item(1);//获得第i个sheet，准备写入
+
+            workSheet.Cells[1, 1] = "1,1";
+            workSheet.Cells[1, 2] = "1,2";
+            workSheet.Cells[1, 3] = "1,3";
+            workSheet.Cells[1, 4] = 4;
+            workSheet.Cells[1, 5] = false;
+            workSheet.Cells[1, 6] = 1.01;
+
+            //set visible the Excel will run in background
+            excelApp.Visible = false;
+            //set false the alerts will not display
+            excelApp.DisplayAlerts = false;
+
+            //workBook.SaveAs(filename, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Excel.XlSaveAsAccessMode.xlNoChange, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            workBook.SaveAs(filename);
+            workBook.Close(false, Missing.Value, Missing.Value);
+
+            //quit and clean up objects
+            excelApp.Quit();
+            workSheet = null;
+            workBook = null;
+            excelApp = null;
+            GC.Collect();
+        }
+
+    }
+}
+
